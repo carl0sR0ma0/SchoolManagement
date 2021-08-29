337d0f711bfe904d43d15296b443a6cf67a55405
rodrigo.fessore <rodrigo.fessore@gmail.com>
2021-08-29 17:27:10 -0300

    Disponibilizando o metodo POST na rota Aluno

---
+++ src/SchoolManagement.API/Controllers/AlunoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Services.Interfaces;
+using SchoolManagement.Services.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunoController : Controller
+    {
+        private readonly IAlunoService service;
+
+        public AlunoController(IAlunoService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost]
+        public IActionResult Post(AlunoCreateViewModel aluno)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+                return Ok(service.Post(aluno));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
