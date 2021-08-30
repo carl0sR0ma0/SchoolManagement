e4a444797a5dba4a16ab96df1da8e911ae4899b8
RockRafael <alves4rafel@outlook.com>
2021-08-29 21:36:25 -0300

    inicio projeto vue

---
+++ src/schoolmanagement.webapp/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Alunos/>
+  </div>
+</template>
+
+<script>
+import Alunos from './components/Alunos.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Alunos
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/schoolmanagement.webapp/src/components/Alunos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{nome}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+    nome: "Alunos",
+    }
+  },
+  props: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
