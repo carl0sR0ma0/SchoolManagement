bad799b3ce3dc8bab9ba794658b349c8c83a37b9
carl0sR0ma0 <cvpromao@gmail.com>
2021-09-17 14:35:34 -0300

    Dando um ajuste no cadastrar aluno

--- src/schoolmanagement.webapp/src/views/Aluno/Cadastro_Aluno.vue
+++ src/schoolmanagement.webapp/src/views/Aluno/Cadastro_Aluno.vue
@@ -1,54 +1,100 @@
 <template>
-<div class="container">
-  <div class="center">
-    <h1>Cadastro de Alunos</h1>
-    <div class="form-floating mb-3">
-      <input v-model="ra" type="text" class="form-control" placeholder="teste" required>
-      <label for="floatingInput">RA do Aluno</label>
-    </div>
-    <div class="form-floating mb-3">
-      <input v-model="nome" type="text" class="form-control" placeholder="teste" required>
-      <label for="floatingInput">Nome</label>
-    </div>
-    <div class="form-floating mb-3">
-      <input v-model="dataNascimento" type="text" class="form-control" placeholder="teste"
-      onfocus="(this.type='date')" onblur="(this.type='text')" required>
-      <label for="floatingInput">Data de Nascimento</label>
-    </div>
-    <div class="form-floating mb-3">
-      <input v-model="rg" type="text" class="form-control" placeholder="teste">
-      <label for="floatingInput">RG</label>
-    </div>
-    <div class="form-floating mb-3">
-      <input v-model="nome" type="text" class="form-control" placeholder="teste">
-      <label for="floatingInput">CPF</label>
-    </div>
-    <div class="form-floating mb-3">
-      <input v-model="telefone" type="text" class="form-control" placeholder="teste">
-      <label for="floatingInput">Telefone</label>
-    </div>
-  <fieldset class="row mb-3">
-    <legend class="col-form-label col-sm-2 pt-0">Sexo</legend>
-    <div class="col-sm-10">
-      <div class="form-check">
-        <input class="form-check-input" type="radio" name="gridRadios" id="sexo0" checked>
-        <label class="form-check-label" for="gridRadios1">
-          Masculino
-        </label>
+  <div class="container">
+    <div class="center">
+      <h1>Cadastro de Alunos</h1>
+      <div class="form-floating mb-3">
+        <input
+          v-model="ra"
+          type="text"
+          class="form-control"
+          placeholder="teste"
+          required
+        />
+        <label for="floatingInput">RA do Aluno</label>
       </div>
-      <div class="form-check">
-        <input class="form-check-input" type="radio" name="gridRadios" id="sexo1">
-        <label class="form-check-label" for="gridRadios2">
-          Feminino
-        </label>
+      <div class="form-floating mb-3">
+        <input
+          v-model="nome"
+          type="text"
+          class="form-control"
+          placeholder="teste"
+          required
+        />
+        <label for="floatingInput">Nome</label>
       </div>
-    </div>
-    </fieldset>
-    <div class="d-grid gap-2">
-    <button type="button" class="btn btn-success" @click="addAluno" >Enviar</button>
+      <div class="form-floating mb-3">
+        <input
+          v-model="dataNascimento"
+          type="text"
+          class="form-control"
+          placeholder="teste"
+          onfocus="(this.type='date')"
+          onblur="(this.type='text')"
+          required
+        />
+        <label for="floatingInput">Data de Nascimento</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          v-model="rg"
+          type="text"
+          class="form-control"
+          placeholder="teste"
+        />
+        <label for="floatingInput">RG</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          v-model="cpf"
+          type="text"
+          class="form-control"
+          placeholder="teste"
+        />
+        <label for="floatingInput">CPF</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          v-model="telefone"
+          type="text"
+          class="form-control"
+          placeholder="teste"
+        />
+        <label for="floatingInput">Telefone</label>
+      </div>
+      <fieldset class="row mb-3">
+        <!-- Corrigir => Sexo para enviar o valor CORRETO -->
+        <legend class="col-form-label col-sm-2 pt-0">Sexo</legend>
+        <div class="col-sm-10">
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="gridRadios"
+              id="sexo0"
+              checked
+            />
+            <label class="form-check-label" for="gridRadios1">
+              Masculino
+            </label>
+          </div>
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="gridRadios"
+              id="sexo1"
+            />
+            <label class="form-check-label" for="gridRadios2"> Feminino </label>
+          </div>
+        </div>
+      </fieldset>
+      <div class="d-grid gap-2">
+        <button type="button" class="btn btn-success" @click="addAluno">
+          Enviar
+        </button>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -65,16 +111,9 @@ export default {
       cpf: "",
       telefone: "",
       sexo: "",
-
-      alunos: [],
     };
   },
-  created() {
-    // this.$http
-    //   .get("https://localhost:5001/Aluno/getAll")
-    //   .then((res) => res.json())
-    //   .then((alunos) => (this.alunos = alunos));
-  },
+  created() {},
 
   methods: {
     addAluno() {
@@ -89,8 +128,7 @@ export default {
       };
 
       axios.post("https://localhost:5001/Aluno/create", _aluno).then((res) => {
-        console.log(res.data.data);
-        this.alunos.push(res.data);
+        console.log(res.data);
       });
 
       this.ra = "";
