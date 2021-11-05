d6e6579227f179eb57be40cc001d7dba60608c4c
carl0sR0ma0 <cvpromao@gmail.com>
2021-11-05 00:06:15 -0300

    Implementando a funcionalidade de realizar a matricula de um aluno em uma turma.

---
+++ src/SchoolManagement.Services/Interfaces/IMatriculaService.cs
@@ -0,0 +1,19 @@
+﻿using SchoolManagement.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Services.Interfaces
+{
+    public interface IMatriculaService
+    {
+        Task<MatriculaDTO> Post(MatriculaDTO matriculaDTO);
+        Task<MatriculaDTO> Update(MatriculaDTO matriculaDTO);
+        Task Remove(long id);
+        Task<MatriculaDTO> Get(long id);
+        Task<List<MatriculaDTO>> Get();
+        Task<MatriculaDTO> GetByRA(long ra);
+    }
+}
