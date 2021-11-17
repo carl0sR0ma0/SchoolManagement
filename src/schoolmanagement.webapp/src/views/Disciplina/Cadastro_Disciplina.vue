caf98e715a6db28e7e0d4f7d39c878f368f8a7f3
carl0sR0ma0 <cvpromao@gmail.com>
2021-11-16 22:58:15 -0300

    Mudando versão do .NET e criando o componente de Cadastro de Disciplina

---
+++ src/schoolmanagement.webapp/src/views/Disciplina/Cadastro_Disciplina.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="center">
+      <h1>Cadastro de Aluno</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cadastro_Disciplina",
+
+  data() {
+    return {};
+  },
+
+  created() {},
+
+  methods: {},
+};
+</script>
+
+<style></style>
